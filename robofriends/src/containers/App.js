d81ac87a0e74d64289426779f781ee1993158aef
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-25 00:40:14 +0000

    files organized

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
-import "./App.css";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "../containers/App.css";
 
 class App extends Component {
   constructor() {
