fde927ab2092d6c144fa81936c3ab6c322e47e7c
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-25 00:56:16 +0000

    scrolling fixed

--- robofriends/src/components/Scroll.js
+++ robofriends/src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div className="overflow-scroll h-screen">
+        <div className="overflow-scroll h-screen scroll">
             {props.children}
         </div>
     )
--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -24,24 +24,21 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1 className="text-center">LOADING</h1>;
-    } else {
-      return (
-        <div className="text-center">
-          <h1 className="text-neutral-700">ROBOFRIENDS</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList classList="inline-block" robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? 
+      <h1 className="text-center">LOADING</h1>
+     : (
+      <div className="text-center">
+        <h1 className="text-neutral-700">ROBOFRIENDS</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList classList="inline-block" robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
