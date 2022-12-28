2de68819b1a677632f7d9b418899d9d93e48e200
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-28 15:35:11 +0000

    deleted uneccessery name atribute

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,10 @@ const Card = ( { id, name, username, email, onCardClick } ) => {
         onCardClick(id);
     }
     return(
-        <div name={id} onClick={handleClick} className='transition cursor-pointer hover:scale-105 bg-blue-100 inline-block p-1 m-5 rounded-lg shadow-xl'>
+        <div onClick={handleClick} className='transition cursor-pointer hover:scale-105 bg-blue-100 inline-block p-1 m-5 rounded-lg shadow-xl'>
             <h2 className='text-center text-lg pt-2 text-neutral-700 username'>{username}</h2>
             <img className='border-2 border-blue-200 rounded-lg' alt = 'robot' src = {`https://robohash.org/${id}?150x150`}/>
-            <div name={id}>
+            <div>
                 <h2 className="text-3xl font-bold text-center text-[#9B7FFD]">{name}</h2>
                 <p className='text-center text-lg text-[#3904FA]'>{email}</p>
             </div>          
